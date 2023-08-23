ec150beec3a3ac14e17b26cc1ff978c373bced39
Priyanshu9898 <priyanshumalaviya9210@gmail.com>
2023-08-23 22:15:13 +0530

    react hot toast added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavbarMenu from './components/NavbarMenu'
+import { Toaster } from 'react-hot-toast'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NavbarMenu />
         {children}
+        <Toaster />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
         </body>
     </html>
---
+++ utils/toastStyle.tsx
@@ -0,0 +1,7 @@
+export const toastStyle = {
+    borderRadius: "10px",
+    background: "#333",
+    color: "#fff",
+  };
+  
+  
